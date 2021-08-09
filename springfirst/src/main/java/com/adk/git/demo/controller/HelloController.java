1c1e26483881fae5db34c1303ff8cdc8b7483dc2
ADKcode <790532173@qq.com>
2021-08-09 20:40:33 +0800

    第一个工程
    springboot的helloworld

---
+++ springfirst/src/main/java/com/adk/git/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.adk.git.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public void hello(){
+        System.out.println("hello");
+    }
+}
